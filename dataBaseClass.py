d39e976b14e5dea8647f589a7f37c0c112c07a6a
Wafaa Daas <w.daas.1999@hotmail.com>
2021-08-15 11:40:45 +0300

    before

--- dataBaseClass.py
+++ dataBaseClass.py
@@ -36,7 +36,7 @@ class DataBase:
         response = []
         result = self.get_data_from_user()
         for i in range(len(result)):
-            if result[i]['idDep'] == idDep or result[i]['type'] == 'head' or result[i]['type'] == 'head of department' :
+            if result[i]['idDep'] == idDep or result[i]['type'] == 'head' or result[i]['type'] == 'head of department':
                 row = dict(
                     idDep=idDep,
                     gender=result[i]['gender'],
